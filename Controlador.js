68c459e0788e54d0bcd6c4578aa487eea6e2b3ec
Diego Cheng <diegocheng1973@gmail.com>
2020-10-16 01:31:36 -0600

    fixed port problem

--- Controlador.js
+++ Controlador.js
@@ -6,11 +6,9 @@ var app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-//For local tests
-//const API_PORT = 3001;
-//app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
-
-var server = app.listen(process.env.PORT || 8080, function(){
-    var port = server.address.port();
-    console.log("App now running on port",port);
+//The local port is 3001
+const API_PORT = process.env.PORT || 3001
+app.listen(API_PORT, function(){
+    console.log("LISTENING ON PORT ",API_PORT);
 })
+
