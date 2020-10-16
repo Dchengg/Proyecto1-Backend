d06327e5ce9b87ea64358920626936a3e481ae73
Diego Cheng <diegocheng1973@gmail.com>
2020-10-16 01:18:54 -0600

    API test

--- Controlador.js
+++ Controlador.js
@@ -1,12 +1,16 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+//For local tests
+//const API_PORT = 3001;
+//app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
+
+var server = app.listen(process.env.PORT || 8080, function(){
+    var port = server.address.port();
+    console.log("App now running on port",port);
+})
