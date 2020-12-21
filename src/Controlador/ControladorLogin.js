16667b22370aec5af55a693e09c4b3eef97a793a
Diego Cheng <diegocheng1973@gmail.com>
2020-12-21 17:53:52 -0600

    login usuarios y roles

--- src/Controlador/ControladorLogin.js
+++ src/Controlador/ControladorLogin.js
@@ -10,19 +10,40 @@ export default class ControladorLogin{
         
     }
 
-    async verificarCombinación(id, pass, tipo){
+    async verificarCombinación(id, pass, idMovimiento){
         try {
-            const res = await this.dao.loginAsesor(id, pass);
-            var idMovimiento;
+            var movimientos = await this.dao.getMovimientos();
+            if(movimientos.find(element => element.id_movimiento == idMovimiento)){
+                var userType = await this.dao.inicioSesion(id, pass, idMovimiento);
+                //await this.creador.cargarMovimiento(idMovimiento);
+                if(userType[0].encontrado){
+                    return "Asesor";
+                }else{
+                    return this.obtenerPermisos(id, idMovimiento);
+                }
+            }else{
+                throw { message: "Movimiento no existe " + idMovimiento}
+            }
+            /*var idMovimiento;
             if (res[0].encontrado) {
                 idMovimiento=await this.creador.cargarMovimiento(id);
-            }
-            Promise.resolve(idMovimiento);
-            res[0].idMovimiento=idMovimiento;
-            return res[0];
+            }*/
+            //this.creador.cargarMovimiento(id);
+            //Promise.resolve(idMovimiento);
+            //res[0].idMovimiento=idMovimiento;
+            //return res[0];
         }
         catch (err) {
             throw err;
         }
     }
+
+    
+    async obtenerPermisos(idMiembro, idMovimiento){
+        var rolesUsuario = await this.dao.getGruposMiembroxMiembro(idMiembro, idMovimiento);
+        var contador = 5;
+        rolesUsuario = rolesUsuario.sort( (a,b) => {return a.id_lider - b.id_lider});
+        return rolesUsuario[0].nombre_lider;
+    }
+
 }
