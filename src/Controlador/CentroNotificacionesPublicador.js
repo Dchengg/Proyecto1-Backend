ac41b391d2d1a37192b32e36c051bda5c9493b69
ArAraya <42824988+AriAraya@users.noreply.github.com>
2020-12-28 17:33:26 -0600

    Base

---
+++ src/Controlador/CentroNotificacionesPublicador.js
@@ -0,0 +1,22 @@
+export default class CentroNotificacionesPublicador{
+    constructor(pDao, pReporteStrategy){
+        this.dao = pDao;
+        this.reporteStrategy = pReporteStrategy;
+    }
+
+    crearNoticia(idNoticia, idZona, idRama, idGrupo, detallesNoticia){
+
+    }
+    
+    notificarReporte(tipo){
+
+    }
+
+    actualizarNotificacionesMiembros(miembros,gestorMiembros){
+
+    }
+
+    obtenerNoticias(idMiembro){
+
+    }
+}
---
+++ src/Modelo/ReporteGeneralStrategy.js
@@ -0,0 +1,5 @@
+export default class ReporteGeneralStrategy{
+    reportar(){
+        
+    }
+}
