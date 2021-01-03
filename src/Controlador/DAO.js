20bbfd02f338518d2194070c980ddd4073a5de92
Diego Cheng <diegocheng1973@gmail.com>
2021-01-02 23:23:10 -0600

    fix 2 consultar noticia

--- src/Controlador/DAO.js
+++ src/Controlador/DAO.js
@@ -805,7 +805,7 @@ const connection = {
     }
 
     async getNoticiaMiembro(idNoticia, idMiembro){
-        return this.client.query("Select * from Noticia inner join noticiasmiembro on Noticia.id_noticia = noticiasmiembro.id_noticia where Noticia.id_noticia="+idNoticia+"AND Noticia.id_miembro='"+idMiembro+"'")
+        return this.client.query("Select Noticia.id_noticia, contenido, Noticia.id_miembro, fecha_publicacion, Noticia.id_movimiento, id_zona, id_rama, id_grupo, nombre, leido from Noticia inner join noticiasmiembro on Noticia.id_noticia = noticiasmiembro.id_noticia where Noticia.id_noticia="+idNoticia+"AND noticiasmiembro.id_miembro='"+idMiembro+"'")
             .then(res => {
                 console.table(res.rows);
                 return res.rows;
